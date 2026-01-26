e4f1f1c6c09495f5ff44d87f6b5cc196dce073c3
Syycc-code <2632093602@qq.com>
2026-01-26 16:01:17 +0800

    refactor: add components barrel export file

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+/**
+ * 组件统一导出文件
+ * 方便在App.tsx中导入使用
+ */
+
+export { RankUpNotification } from './notifications/RankUpNotification';
+export { TransitionCurtain, ShatterEffect } from './transitions';
+export { StatsOverlay } from './overlays/StatsOverlay';
